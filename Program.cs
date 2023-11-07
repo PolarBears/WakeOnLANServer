d14683f399b111e78d7c8917b0ce4509509ebf71
PolarBears <polarbearscn@outlook.com>
2023-11-07 10:25:18 +0800

    增加首页网页

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
         var app = builder.Build();
 
-        app.MapGet("/", () => "Hello World!");
+        app.UseDefaultFiles();
+        app.UseStaticFiles();
 
         app.Run();
     }
